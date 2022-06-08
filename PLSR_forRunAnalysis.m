d73d981584b0293ea1e9f18ae4e2b80f1043d074
Sarah West <west0883@umn.edu>
2022-06-07 20:30:53 -0500

    Tests a maximal number of components, then re-runs regressions based on minimum MSE.

--- PLSR_forRunAnalysis.m
+++ PLSR_forRunAnalysis.m
@@ -8,10 +8,29 @@ function [parameters] = PLSR_forRunAnalysis(parameters)
 
     MessageToUser('Regressing ', parameters);
 
-    % Run plsregress
-    [results.XL, results.YL, results.XS, results.YS, results.BETA, results.PCTVAR, results.MSE, results.stats] ...
-       = plsregress(parameters.explanatory, parameters.response, parameters.numComponents, 'cv', 5, 'Options', statset('UseParallel',true) );
+    % Run plsregress, using a maximal number of components (somewhat
+    % arbitrary -- start with 20)
+    ncomponents_original = 20; 
+
+    [~, ~, ~, ~, ~, ~, MSE_original, stats_original] ...
+       = plsregress(parameters.explanatory, parameters.response, ncomponents_original, 'cv', 5, 'Options', statset('UseParallel',true) );
     
+    % Save the original weights of Y for later (in case you want to look at
+    % what those components look like later)
+    W_original = stats_original.W;
+
+    % Find component with minimum MSE
+    [ncomponents] = min(MSE_original);
+
+    % Now run with optimal number of components.
+    [results.XL, results.YL, results.XS, results.YS, results.BETA, results.PCTVAR, results.MSE, results.stats] ...
+       = plsregress(parameters.explanatory, parameters.response, ncomponents, 'cv', 5, 'Options', statset('UseParallel',true) );
+    
+    % Put MSE_original, ncomponents, & W_original into the results.
+    results.maximal_components.MSE = MSE_original;
+    results.maximal_components.W = W_original;
+    results.best_ncomponents = ncomponents; 
+
     % Put into output structure;
     parameters.results = results; 
 
