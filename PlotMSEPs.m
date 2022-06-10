aa07bc9e80ddf616afe70766721089aab08415e1
Sarah West <west0883@umn.edu>
2022-06-10 12:00:21 -0500

    Plot each mouse's MSEPs for each comparison up to 21 components.

---
+++ PlotMSEPs.m
@@ -0,0 +1,44 @@
+% PlotWeights.m
+% Sarah West 
+% 6/9/22
+
+% Plot a number of MSEPs from partial least squares regression. Run by RunAnalysis.
+
+function [parameters] = PlotMSEPs(parameters)
+
+ 
+
+    if ~isfield(parameters,'xfig')
+        parameters.xfig = figure;
+        parameters.xfig.WindowState = 'maximized';
+        parameters.x_axis = gca;
+        title_string = ['MSEPs for explanatory variables ' parameters.values(1)];
+        title(title_string);
+        axis tight;
+        xlabel('component number');
+        ylabel('MSEP');
+
+        hold on; 
+    end
+
+    hold on; 
+
+
+    if ~isfield(parameters,'yfig')
+        parameters.yfig = figure;
+        parameters.yfig.WindowState = 'maximized';
+      
+        parameters.y_axis = gca;
+        title_string = ['MSEPs for response variables ' parameters.values(1)];
+        title(title_string);
+        axis tight;
+        xlabel('component number');
+        ylabel('MSEP');
+
+        hold on; 
+    end
+    hold on;
+    
+    plot(parameters.x_axis, parameters.results.maximal_components.MSEP(1,:));
+    plot(parameters.y_axis, parameters.results.maximal_components.MSEP(2,:));
+end 
