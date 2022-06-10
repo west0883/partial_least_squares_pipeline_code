6009afa8617c67eb49cbbf607b7107af201a4527
Sarah West <west0883@umn.edu>
2022-06-10 11:54:54 -0500

    Improvements.

--- PlotWeights.m
+++ PlotWeights.m
@@ -6,31 +6,31 @@
 
 function [parameters] = PlotWeights(parameters)
 
-    [subplot_rows, subplot_columns] = OptimizeSubplotNumbers(numel(parameters.components_to_plot),4/5);
+     weights = parameters.results.stats.W; 
 
-    indices = logical(tril(ones(parameters.number_of_sources), -1));
-
-    weights = parameters.results.stats.W; 
+    [subplot_rows, subplot_columns] = OptimizeSubplotNumbers(size(weights, 2),4/5);
 
     fig = figure;
     fig.WindowState = 'maximized';
-    for componenti = parameters.components_to_plot
+    for componenti = 1:size(weights, 2)
 
         holder = NaN(parameters.number_of_sources, parameters.number_of_sources);
 
         % First row is constant estimate
-        holder(indices) = weights(:, componenti);
+        holder(parameters.indices) = weights(:, componenti);
 
         % Get most negative or positive value for color range, center
         % around 0.
         extreme = max(max(holder, [], 'all', 'omitnan'), abs(min(holder, [], 'all', 'omitnan')));
         color_range = [-extreme extreme]; 
 
-        subplot(subplot_rows, subplot_columns, componenti); imagesc(holder); colorbar; caxis(color_range)
+        subplot(subplot_rows, subplot_columns, componenti); imagesc(holder); colorbar;  caxis(color_range)
         title(num2str(componenti)); axis square;
 
     end
-    sgtitle(['Weights' strjoin(parameters.values(1:numel(parameters.values)/2), ', ')])
+    title_string = ['Weights ' strjoin(parameters.values(1:numel(parameters.values)/2), ', ')];
+    title_string = strrep(title_string, '_', ' ');
+    sgtitle(title_string);
 
     % Put into output structure.
     parameters.fig = fig;
