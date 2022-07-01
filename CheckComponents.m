9cfa717780c5c2b39d22af002023bc710082c1b6
Sarah West <west0883@umn.edu>
2022-07-01 10:05:27 -0500

    Each segment optional, add BIC plots.

--- CheckComponents.m
+++ CheckComponents.m
@@ -2,7 +2,7 @@
 % Sarah West
 % 6/16/22
 
-% Runs PlotWeights, PlotPCTVAR, & PlotMSEPs after a run of PLSR that looked 
+% Runs PlotWeights, PlotPCTVAR, PlotMSEPs, PlotBICs after a run of PLSR that looked 
 % for best number of components with cross validation. Run with
 % RunAnalysis.
 
@@ -25,53 +25,83 @@ function [parameters] = CheckComponents(parameters)
       end
       
       %  **** Plot explanatory & response MSEPs 
-      
-      % Rename input figure handles, explanatory
-      if isfield(parameters, 'fig_MSEPs_explanatory')
-          parameters.xfig = parameters.fig_MSEPs_explanatory;
-
-      elseif isfield(parameters, 'xfig')
-          parameters = rmfield(parameters, 'xfig');
+      % If user says so
+      if isfield(parameters, 'plot_MSEPs') && parameters.plot_MSEPs
+          % Rename input figure handles, explanatory
+          if isfield(parameters, 'fig_MSEPs_explanatory')
+              parameters.xfig = parameters.fig_MSEPs_explanatory;
+    
+          elseif isfield(parameters, 'xfig')
+              parameters = rmfield(parameters, 'xfig');
+          end
+    
+           % Rename input figure handles, response
+          if isfield(parameters, 'fig_MSEPs_response')
+              parameters.yfig = parameters.fig_MSEPs_response;
+    
+          elseif isfield(parameters, 'yfig')
+              parameters = rmfield(parameters, 'yfig');
+          end
+          % Plot.
+          parameters = PlotMSEPs(parameters);
+    
+          % Rename output figure handles. 
+          parameters.fig_MSEPs_explanatory = parameters.xfig;
+          parameters.fig_MSEPs_response = parameters.yfig;
       end
 
-       % Rename input figure handles, response
-      if isfield(parameters, 'fig_MSEPs_response')
-          parameters.yfig = parameters.fig_MSEPs_response;
-
-      elseif isfield(parameters, 'yfig')
-          parameters = rmfield(parameters, 'yfig');
+      % **** Plot BICs 
+      % If user says so
+      if isfield(parameters, 'plot_BICs') && parameters.plot_BICs
+          % Rename input figure handles, explanatory
+          if isfield(parameters, 'fig_BICs_explanatory')
+              parameters.xfig = parameters.fig_BICs_explanatory;
+    
+          elseif isfield(parameters, 'xfig')
+              parameters = rmfield(parameters, 'xfig');
+          end
+    
+           % Rename input figure handles, response
+          if isfield(parameters, 'fig_BICs_response')
+              parameters.yfig = parameters.fig_BICs_response;
+    
+          elseif isfield(parameters, 'yfig')
+              parameters = rmfield(parameters, 'yfig');
+          end
+          % Plot.
+          parameters = PlotBICs(parameters);
+    
+          % Rename output figure handles. 
+          parameters.fig_BICs_explanatory = parameters.xfig;
+          parameters.fig_BICs_response = parameters.yfig;
       end
-      % Plot.
-      parameters = PlotMSEPs(parameters);
 
-      % Rename output figure handles. 
-      parameters.fig_MSEPs_explanatory = parameters.xfig;
-      parameters.fig_MSEPs_response = parameters.yfig;
-      
+
       % **** Plot explanatory & response percent variance 
 
-      
-      % Rename input figure handles, explanatory
-      if isfield(parameters, 'fig_PCTVARs_explanatory')
-          parameters.xfig = parameters.fig_PCTVARs_explanatory;
-
-      elseif isfield(parameters, 'xfig')
-          parameters = rmfield(parameters, 'xfig');
+      % If user says so.
+      if isfield(parameters, 'plot_percentVars') && parameters.plot_percentVars
+          % Rename input figure handles, explanatory
+          if isfield(parameters, 'fig_PCTVARs_explanatory')
+              parameters.xfig = parameters.fig_PCTVARs_explanatory;
+    
+          elseif isfield(parameters, 'xfig')
+              parameters = rmfield(parameters, 'xfig');
+          end
+          % Rename input figure handles, response
+          if isfield(parameters, 'fig_PCTVARs_response')
+              parameters.yfig = parameters.fig_PCTVARs_response;
+    
+          elseif isfield(parameters, 'yfig')
+              parameters = rmfield(parameters, 'yfig');
+          end
+    
+          % Plot
+          parameters = PlotPCTVAR(parameters);
+    
+          % Rename output figure handle. 
+          parameters.fig_PCTVARs_explanatory = parameters.xfig;
+          parameters.fig_PCTVARs_response = parameters.yfig;
       end
-      % Rename input figure handles, response
-      if isfield(parameters, 'fig_PCTVARs_response')
-          parameters.yfig = parameters.fig_PCTVARs_response;
-
-      elseif isfield(parameters, 'yfig')
-          parameters = rmfield(parameters, 'yfig');
-      end
-
-      % Plot
-      parameters = PlotPCTVAR(parameters);
-
-      % Rename output figure handle. 
-      parameters.fig_PCTVARs_explanatory = parameters.xfig;
-      parameters.fig_PCTVARs_response = parameters.yfig;
-
                 
 end 
