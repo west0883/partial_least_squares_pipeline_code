1235ddffb938195e22c49481db150fd5f3f82484
Sarah West <west0883@umn.edu>
2022-07-06 09:28:13 -0500

    Correct output format for maximal components PLSR.

--- PLSR_forRunAnalysis.m
+++ PLSR_forRunAnalysis.m
@@ -282,8 +282,8 @@ function [parameters] = PLSR_forRunAnalysis(parameters)
             % weights and stuff), run it.
             if isfield(parameters, 'run_with_max_components') && parameters.run_with_max_components
                 disp('Running with max components.');
-                 [results.maximal_components.maximal_components.XL, results.maximal_components.YL, results.maximal_components.XS, results.maximal_components.YS, results.maximal_components.BETA, ...
-                       results.maximal_components.stats] = plsregress_fullcode(explanatoryVariables, responseVariables, ncomponents_max); 
+                 [results.maximal_components.XL, results.maximal_components.YL, ~, ~, results.maximal_components.BETA, ~, ...
+                       results.maximal_components.MSE, results.maximal_components.stats] = plsregress_fullcode(explanatoryVariables, responseVariables, ncomponents_max); 
             end
 
         % If not contguous partitions, run with random paritions.
