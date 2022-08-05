da1774bb4a25a739d02ced23e1d305ce5e2dbdac
Sarah West <west0883@umn.edu>
2022-08-05 12:00:35 -0500

    Update to use for warning Periods, option for no motorized static.

--- PopulateResponseVariables.m
+++ PopulateResponseVariables.m
@@ -50,43 +50,58 @@ function [parameters] = PopulateResponseVariables(parameters)
         % Put into structure.
         response_variables_structure.pupil_diameter_vector = diameters;
 
-        % If this period is motorized, 
-        if strcmp(parameters.periods{periodi, 'motorized_vs_spon'}{1}, 'motorized')
-            % For each of the motorized-specific static categories.
-            for statici = 1:numel(parameters.motorized_variables_static)
-            
-                % Replicate by number of instancs in 3rd dimension.
-                response_variables_structure.(parameters.motorized_variables_static{statici}) = repmat(parameters.periods{periodi, parameters.motorized_variables_static{statici}}{1}, 1, 1, instances);
-            end 
+        % Skip these steps if there's no motorized_variables_static field,
+        % just replicate the speed & accel vectors of all periods.
+        if isfield(parameters, 'motorized_variables_static')
+        
+            % If this period is motorized, 
+            if strcmp(parameters.periods{periodi, 'motorized_vs_spon'}{1}, 'motorized')
+                % For each of the motorized-specific static categories.
+                for statici = 1:numel(parameters.motorized_variables_static)
+                
+                    % Replicate by number of instancs in 3rd dimension.
+                    response_variables_structure.(parameters.motorized_variables_static{statici}) = repmat(parameters.periods{periodi, parameters.motorized_variables_static{statici}}{1}, 1, 1, instances);
+                end 
+        
+            % Else, this period is spontaneous 
+            else 
+                % Get the condition name 
+                condition = parameters.periods{periodi, 'condition'}{1};
     
-        % Else, this period is spontaneous 
-        else 
-            % Get the condition name 
-            condition = parameters.periods{periodi, 'condition'}{1};
+                % Find location in the speed & accel vectors
+                location = logical(cellfun(@strcmp, parameters.spontaneous_periods_order', ...
+                    repmat({condition},numel(parameters.spontaneous_periods_order), 1))); 
+    
+                % For each of the motorized-specific static categories.
+                for statici = 1:numel(parameters.motorized_variables_static)
+    
+                   % Get out the needed values.
+                   values = parameters.(parameters.motorized_variables_static{statici}){location};
+    
+                   % Take absolute value. (So you don't get negative speeds or
+                   % accels. I don't want 0 to be in the middle of the curve).
+                   values = abs(values);
+    
+                   % Permute the values to make it match the format of all the
+                   % other response variables. (put in a 1 at the beginning for the number of variables)
+                   values = permute(values, [3, 1, 2]);  % Use "3" as the first dimension, because it's a 1.
+    
+                   % Check if dim 3 == instances
+    
+                   % Put into structure.
+                   response_variables_structure.(parameters.motorized_variables_static{statici}) = values;
+                
+                end 
+            end
 
-            % Find location in the speed & accel vectors
-            location = logical(cellfun(@strcmp, parameters.spontaneous_periods_order', ...
-                repmat({condition},numel(parameters.spontaneous_periods_order), 1))); 
-
-            % For each of the motorized-specific static categories.
-            for statici = 1:numel(parameters.motorized_variables_static)
-
-               % Get out the needed values.
-               values = parameters.(parameters.motorized_variables_static{statici}){location};
-
-               % Take absolute value. (So you don't get negative speeds or
-               % accels. I don't want 0 to be in the middle of the curve).
-               values = abs(values);
-
-               % Permute the values to make it match the format of all the
-               % other response variables. (put in a 1 at the beginning for the number of variables)
-               values = permute(values, [3, 1, 2]);  % Use "3" as the first dimension, because it's a 1.
-
-               % Check if dim 3 == instances
-
-               % Put into structure.
-               response_variables_structure.(parameters.motorized_variables_static{statici}) = values;
-            
+        % If no motorized static, just replicate all speeds & accels
+        else
+            variables = {'speed_vector', 'accel_vector'};
+            for variablei = 1:numel(variables)
+                variable = variables{variablei};
+                
+                % Replicate by number of instancs in 3rd dimension.
+                response_variables_structure.(variable) = repmat(parameters.periods{periodi, variable{1}, 1, 1, instances);
             end 
         end
 
@@ -111,7 +126,6 @@ function [parameters] = PopulateResponseVariables(parameters)
             end
 
         end
-    end
 
     % If not vertically concatenated, make each column of
     % response_variables_cellholder its own variable in the
